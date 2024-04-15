378f47e2f2a96f1c3fb354fde5a549e9a4623947
Mahammad Handamzada <mehemmedhendemzadeh@gmail.com>
2024-04-15 10:06:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using TextFile;
+namespace SAMPLE2
+{
+    public class DataLine
+    {
+        public int ID;
+        public int WORK;
+        public DataLine(int ID, int WORK) { 
+        this.ID = ID;
+        this.WORK = WORK;}
+    }
+
+    internal class Program
+    {
+        public static DataLine? ReadData(TextFileReader reader)
+        {
+            bool ReadID = reader.ReadInt(out int  ID);
+            bool ReadWORK = reader.ReadInt(out int  WORK);
+            if(ReadID && ReadWORK)
+            {
+                return new DataLine(ID, WORK);
+            }
+            else
+            {
+                return null;
+            }
+        }
+            
+    
+          
+        static void Main(string[] args)
+        {
+            var reader = new TextFileReader("input.txt");
+            DataLine? line = null;  
+            var max = 0;
+            var maxId = 0;
+            while((line= ReadData(reader)) != null)
+            {
+                if (line.WORK > max)
+                {
+                    max = line.WORK;
+                    maxId = line.ID;
+
+                }   
+            }
+            Console.WriteLine(maxId);
+        }
+    }
+}
